34b2e34908cdc5a9c378f25ebcc651fb0d0576f3
Ishaq Hassan <ishaquehassan@gmail.com>
2019-11-02 18:06:44 +0500

    Class code of 2 nov 2019

---
+++ 2Nov2019/Array.dart
@@ -0,0 +1,12 @@
+void main() {
+  String friend1 = "Ishaq";
+  String friend2 = "Ali";
+  String friend3 = "Usama";
+  List<String> friends = [
+    "Ishaq","Ali","Usama","A"
+  ];
+  print(friends[0]);
+  print(friends[1]);
+  print(friends[2]);
+  print(friends[3]);
+}
---
+++ 2Nov2019/BasicVariables.dart
@@ -0,0 +1,17 @@
+void main() {
+  int x;
+  x = 20;
+  int a = 10;
+  double b = 1.2;
+  bool good = true;
+  String name = "Ishaq";
+  //char abc = 'a';
+  int c = a + x;
+  print(c);
+  print(x);
+  print(a);
+  print(b);
+  print(good);
+  print(name);
+  print("Hello world");
+}
